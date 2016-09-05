3eb36a8bdcab9b0aa4192d1de38567ff433778e9
hsufelix-git <hsufelix@yeah.net>
2016-09-06 01:25:25 +0800

    修改为ejs模板
    修改为ejs模板

--- blog/app.js
+++ blog/app.js
@@ -13,8 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 var ejs =  require('ejs');
-app.engine('.html',ejs.__express);
-app.set('view engine', 'html');
+app.engine('.ejs',ejs.__express);
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', routes);
 // app.use('/users', users);
 routes(app);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,6 +59,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -9,16 +9,40 @@
 
 module.exports = function(app) {
 
+	/**
+	 * 路由配置例子
+
+		app.get('/', function (req, res, next) {
+			//  使用render就是要使用ejs模板引擎
+			//  第一个参数index就是index.ejs
+			//  title就是改页面里面的所有title变量的值都是Express
+			res.render('index', { title: 'Expressss' });
+		});
+
+		app.get('/felix', function (req, res, next) {
+			// 使用send则直接输出不需要使用模板引擎ejs或jade
+			res.send("hello felix");
+		});
+	*/
+	console.info('get in router');
+
 	app.get('/', function (req, res, next) {
-		//  使用render就是要使用ejs模板引擎
-		//  第一个参数index就是index.ejs
+		//  使用render就是要使用ejs模板引擎（现在使用的模板后缀是html）
+		//  第一个参数index就是index.ejs（现在使用的模板后缀是index.html）
 		//  title就是改页面里面的所有title变量的值都是Express
 		res.render('index', { title: 'Expressss' });
 	});
 
-	app.get('/felix', function (req, res, next) {
-		// 使用send则直接输出不需要使用模板引擎ejs或jade
-		res.send("hello felix");
+	app.get('/login', function (req, res, next) {
+		res.send('login');
+	});
+
+	app.get('/logout', function (req, res, next) {
+		res.send('logout');
+	});
+
+	app.get('/register', function (req, res, next) {
+		res.send('register');
 	});
 };
 
